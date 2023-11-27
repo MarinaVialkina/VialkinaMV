78708a572d9c5bc26185ca1388dc533fc83dd761
MarinaVialkina <145276786+MarinaVialkina@users.noreply.github.com>
2023-11-27 20:19:42 +0300

    Исправила

--- 11_PySimpleGui/v3_json.py
+++ 11_PySimpleGui/v3_json.py
@@ -2,11 +2,9 @@ import requests
 from pprint import pprint
 import json
 
+
 def get_json():
     username = "VisualStudioCode"
     url = f"https://api.github.com/users/{username}"
     user_data = requests.get(url).json()
     return user_data
-print(get_json())
-
-
